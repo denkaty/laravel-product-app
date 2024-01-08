e09395b49f5b489edcf61a830842e6ddf0338a49
denkaty <denisselim2003@gmail.com>
2024-01-09 00:07:23 +0200

    Update 'web.php': add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/Home', function () {
+    return view('index');
+});
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/search', [CategoryController::class, 'search'])->name('categories.search');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
