b96f582c89e8b106ca83645344194f8a10a76141
denkaty <denisselim2003@gmail.com>
2024-01-11 22:16:21 +0200

    Refactoring 'ProductController'

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,30 +9,25 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $sort = $request->input('sort');
+        $sort = htmlspecialchars($request->input('sort'));
+        $searchTerm = htmlspecialchars($request->input('search'));
+
+        $products = [];
 
         if ($sort === 'name') {
             $products = Product::orderBy('name')->get();
-            return view('products.sort_by_name', compact('products'));
+            return view('products.index', compact('products'));
         }
 
-        $products = Product::orderBy('created_at', 'desc')->get();
+        if (empty($searchTerm)) {
+            $products = Product::orderBy('created_at', 'desc')->get();
+            return view('products.index', compact('products'));
+        }
+
+        if (!preg_match('/^[%_]+$/', $searchTerm)) {
+            $products = Product::search($searchTerm)->get();
+        }
 
         return view('products.index', compact('products'));
-    }
-
-    public function search(Request $request)
-    {
-        $searchTerm = htmlspecialchars($request->input('search'));
-        if (empty($searchTerm)) {
-            return redirect()->route('products.index');
-        }
-        if (preg_match('/^[%_]+$/', $searchTerm)) {
-            return redirect()->route('products.index');
-        }
-
-        $products = Product::search($searchTerm)->get();
-
-        return view('products.search', compact('products'));
     }
 }
